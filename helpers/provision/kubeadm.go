6a847478750b397766cfd0aae25a232b02daa330
vitu <vitumafeni@dcn.ssu.ac.kr>
2026-05-26 23:10:44 +0900

    applying crd to client control-plane

--- helpers/provision/kubeadm.go
+++ helpers/provision/kubeadm.go
@@ -143,6 +143,10 @@ https://download.opensuse.org/repositories/isv:/cri-o:/stable:/v%s/deb/ /" \
 		"rm /tmp/catalog/ -rf",
 		"git clone https://github.com/vitu-mafeni/catalog.git /tmp/catalog",
 		"kubectl apply -f /tmp/catalog/nephio/optional/flux-helm-controllers",
+
+		// temporary, will be removed after the controller is containerized.
+		"git clone -b r2-1 https://github.com/vitu-mafeni/remote-cluster-provisioner.git /tmp/remote-cluster-provisioner",
+		"kubectl apply -f /tmp/remote-cluster-provisioner/config/samples",
 	}
 
 	for _, cmd := range steps {
